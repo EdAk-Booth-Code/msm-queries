e68c80a31f94c2b500436c6214fde0ea822aa798
EdAk-Booth <edwardacosta@chicagobooth.edu>
2021-06-03 04:53:47 +0000

    Created logic to show youngest and eldest directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,7 +1,25 @@
 class DirectorsController < ApplicationController 
 
   def index
+
+    @list_of_directors = Director.all
+
     render({ :template => "director_templates/index.html.erb"})
+  end
+
+  def show_eldest
+    
+    @eldest_director = Director.order({ :dob => :asc}).where.not({ :dob => nil}).at(0)  
+    render({ :template => "director_templates/eldest.html.erb"})
+ 
+
+  end
+
+  def show_youngest
+
+    @youngest_director = Director.order({ :dob => :asc}).where.not({ :dob => nil}).at(-1)  
+    render({ :template => "director_templates/youngest.html.erb"})
+
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index"})
+
+  get("/directors/eldest", { :controller => "directors", :action => "show_eldest"})
+
+  get("/directors/youngest", { :controller => "directors", :action => "show_youngest"})
+
 end
